10dcca509a4bba753430278fd3116433e5c243d4
shubhu1112 <Shubhu0899@gmail.com>
2025-06-17 17:19:03 +0530

    Add all project files

---
+++ src/backend/crud.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+import models
+import schemas
+from passlib.hash import bcrypt
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_pw = bcrypt.hash(user.password)
+    db_user = models.User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        hashed_password=hashed_pw,
+        role=user.role
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
