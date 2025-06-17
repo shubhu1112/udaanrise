80c8eb979270ad524fc45d2f2341273c569efe9a
shubhu1112 <80643208+shubhu1112@users.noreply.github.com>
2025-06-17 19:07:44 +0530

    Add files via upload

---
+++ src/backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from src.backend.routes import auth
+from fastapi.middleware.cors import CORSMiddleware
+from src.backend.database import engine
+from src.backend import models
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# ✅ Allow frontend to access backend (from browser)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify frontend URL here for more security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth.router)
